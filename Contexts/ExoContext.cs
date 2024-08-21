5f2f2c405d823604d9ccbd11f143dbdf85ddebb9
ArchLemos <51244569+CabeludoCego@users.noreply.github.com>
2024-08-21 12:09:56 -0300

    ✨C#_WebApi - v2 CRUD ok

---
+++ Contexts/ExoContext.cs
@@ -0,0 +1,33 @@
+using Exo.WebApi.Models;
+using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+
+namespace Exo.WebApi.Contexts
+{
+	public class ExoContext : DbContext
+	{
+		public ExoContext() { }
+		public ExoContext(DbContextOptions<ExoContext> options) : base(options) { }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			if (!optionsBuilder.IsConfigured)
+			{
+
+// SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder(
+//                     "User Id=sa;Password=1234;" +
+//                     "Server=LAPTOP-A5LLEMOS;" +
+//                     "Database=vendas;" +
+//                     "Trusted_Connection=False;" );
+
+
+				// Essa string de conexão depende da SUA máquina.
+				optionsBuilder.UseSqlServer("Server=LAPTOP-A5LLEMOS;"
+				+ "Database=ExoApi;Trusted_Connection=False;"
+				+  "User Id=sa;Password=1234;" );
+			}
+		}
+		public DbSet<Projeto> Projetos { get; set; }
+	}
+}
